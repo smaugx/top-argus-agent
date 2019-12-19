93a8d3e568fca79eee68e032ad9753ce1d341155
smaug.jiao <smaug.jiao@topnetwork.org>
2019-12-19 17:38:44 +0800

    update common base

--- common/slogging.py
+++ common/slogging.py
@@ -2,24 +2,37 @@
 #-*-coding:utf8 -*-
 
 import logging,os
+import common.config as sconfig
 
 base_path = './log'
 pid= os.getpid()
-#TODO(smaug)
 pid = 'xx'
-path = os.path.join(base_path, 'xx-{0}.log'.format(pid))
+path = os.path.join(base_path, 'topargus-{0}.log'.format(pid))
 if not os.path.exists(base_path):
     os.mkdir(base_path)
 
 slog = logging.getLogger(path)
-slog.setLevel(logging.DEBUG)
+
+if sconfig.LOGLEVEL == 'debug':
+    slog.setLevel(logging.DEBUG)
+elif sconfig.LOGLEVEL == 'info':
+    slog.setLevel(logging.INFO)
+elif sconfig.LOGLEVEL == 'warn':
+    slog.setLevel(logging.WARNING)
+elif sconfig.LOGLEVEL == 'error':
+    slog.setLevel(logging.ERROR)
+elif sconfig.LOGLEVEL == 'critical':
+    slog.setLevel(logging.CRITICAL)
+else:
+    slog.setLevel(logging.DEBUG)
+
 '''
 %(filename)s        调用日志输出函数的模块的文件名
 %(module)s          调用日志输出函数的模块名
 %(funcName)s        调用日志输出函数的函数名
 %(lineno)d          调用日志输出函数的语句所在的代码行
 '''
-fmt = logging.Formatter('[%(asctime)s][%(thread)d][%(levelname)s][%(filename)s][%(funcName)s][%(lineno)d]:%(message)s', '%Y-%m-%d %H:%M:%S')
+fmt = logging.Formatter('[%(asctime)s][%(process)d][%(thread)d][%(levelname)s][%(filename)s][%(funcName)s][%(lineno)d]:%(message)s', '%Y-%m-%d %H:%M:%S')
 
 #设置CMD日志
 sh = logging.StreamHandler()
