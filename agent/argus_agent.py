b8fa917d07e067b2b9d1c872cff128d68afa9a0a
smaugx <linuxcode2niki@gmail.com>
2020-01-09 11:29:32 +0000

    fix uniq_chain_hash

--- agent/argus_agent.py
+++ agent/argus_agent.py
@@ -230,8 +230,10 @@ def grep_log_broadcast(line):
 
         chain_hash = int(packet_info.get('chain_hash'))
         #uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('src_node_id')[-10:], packet_info.get('dest_node_id')[-10:]) # TODO(smaug) for 01000 broadcast, dest_node_id maybe changed
-        uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('src_node_id')[-10:], packet_info.get('chain_msgid'))
+        #uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('src_node_id')[-10:], packet_info.get('chain_msgid'))
+        uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('chain_msgid'), packet_info.get('chain_msg_size'))
         uniq_chain_hash = int(hashlib.sha256(uniq_key.encode('utf-8')).hexdigest(),16)  % ( 10 ** 19)
+        slog.info('testtest uniq_key:{0} hash:{1}'.format(uniq_key, uniq_chain_hash))
         packet_info['uniq_chain_hash'] = uniq_chain_hash
         rn = uniq_chain_hash % 1000 + 1  # [1,1000]
         if rn > sample_rate:
@@ -429,8 +431,8 @@ def grep_log_point2point(line):
 
         chain_hash = int(packet_info.get('chain_hash'))
         uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('src_node_id')[-10:], packet_info.get('dest_node_id')[-10:])
-        uniq_key = '{0}_{1}_{2}'.format(chain_hash, packet_info.get('chain_msgid'), packet_info.get('chain_msg_size'))
         uniq_chain_hash = int(hashlib.sha256(uniq_key.encode('utf-8')).hexdigest(),16)  % ( 10 ** 19)
+        slog.info('testtest uniq_key:{0} hash:{1}'.format(uniq_key, uniq_chain_hash))
         packet_info['uniq_chain_hash'] = uniq_chain_hash
         rn = uniq_chain_hash % 1000 + 1  # [1,1000]
         if rn > sample_rate:
